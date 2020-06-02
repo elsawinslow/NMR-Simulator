31205934ebc140629b34c7c10c803996ab9199df
Elsa Winslow <elsawinslow@Macbook-Pro-2.local>
2020-06-02 13:40:04 -0700

    added UI

--- NMRSimulator.py
+++ NMRSimulator.py
@@ -1,11 +1,15 @@
 import math
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import numpy as np
 from scipy import *
 from scipy.integrate import quad
+from matplotlib.widgets import TextBox, Button
+from matplotlib.gridspec import GridSpec
+#from decimal import Decimal
 
-from decimal import Decimal
+
 
 import os.path
 from os import path
@@ -17,28 +21,18 @@ pi = math.pi
 
 def user_var_input():
     filename = "parameters.txt"
-    change_val = "*"
     if path.exists(filename):
         para= open(filename,"r")
         para_lines = para.readlines()
-        var.LAMBDA = int(para_lines[0])
-        var.M0 = int(para_lines[1])
-        var.ALPHA = int(para_lines[2])
-        var.OMEGA = int(para_lines[3])
-        var.R2 = int(para_lines[4])
-        print("------Previous parameters------ \nLambda = " + para_lines[0] + "M naught = " + para_lines[1] + "Alpha = " + para_lines[2] + "Omega = " + para_lines[3] + "R2 = " + para_lines[4] + "")
-        change_val = input("---------To change enter-------- \nL for lambda \nM for M naught \nA for alpha \nO for Omega \nR for R2 \n* for all values \nExample: LMO \nenter for no changes: \n").lower()
-    if "l" in change_val or "*" in change_val:     
-        var.LAMBDA = int(input("Input value for lambda: "))  
-    if "m" in change_val or "*" in change_val:    
-        var.M0 = int(input("Input value for M naught: "))
-    if "a" in change_val or "*" in change_val:       
-        var.ALPHA = int(input("Input value for alpha (degrees): ")) 
-    if "o" in change_val or "*" in change_val:    
-        var.OMEGA = int(input("Input value for (capital) omega (degrees): "))
-    if "r" in change_val or "*" in change_val:    
-        var.R2 = int(input("Input value for R2: "))
-    
+        var.LAMBDA = float(para_lines[0])
+        var.M0 = float(para_lines[1])
+        var.ALPHA = float(para_lines[2])
+        var.OMEGA = float(para_lines[3])
+        var.R2 = float(para_lines[4])
+    event = "event"
+
+    plot(event)
+
     para = open(filename,"w")
     para.write(str(var.LAMBDA)+"\n")
     para.write(str(var.M0)+"\n")
@@ -46,8 +40,7 @@ def user_var_input():
     para.write(str(var.OMEGA)+"\n")
     para.write(str(var.R2)+"\n")
     para.close()
-    var.ALPHA = np.radians(var.ALPHA)
-    var.OMEGA = np.radians(var.OMEGA)
+
     
 def signal_of_time(t):
     return var.LAMBDA*var.M0*np.sin(var.ALPHA)*np.exp(1j*var.OMEGA*t-var.R2*t) 
@@ -89,7 +82,6 @@ def plot_frequency():
 
     ax.grid()
 
-    fig.savefig("test.png")
     plt.show()
 
 def plot_time():
@@ -99,7 +91,7 @@ def plot_time():
     fig, ax = plt.subplots()
     ax.plot(t, np.real(signal), label = 'real')
     ax.plot(t, np.imag(signal),'r', label = 'imaginary')
-    res = ": Off Resonace"
+    res = ": Off Resonance"
     if var.OMEGA == 0:
         res = ": On Resonance"
     ax.set(xlabel='Time (s)', ylabel='', title='FID'+res)
@@ -107,11 +99,68 @@ def plot_time():
 
     ax.grid()
 
-    fig.savefig("test.png")
     plt.show()
 
+def plot(event):
+    t = np.arange(0, 5, .2)
+    omega_vals = np.arange(-5, 5, 0.2)
+
+    signal1 = signal_of_time(t)
+    signal2 = integrate(omega_vals)
+
+    res = ": Off Resonance"
+    if var.OMEGA == 0:
+        res = ": On Resonance"
+
+    plt.subplot2grid((2, 3), (0, 0), colspan=2)
+    plt.plot(t, np.real(signal1), label = 'real')
+    plt.plot(t, np.imag(signal1), "r", label = "imaginary" )
+    plt.title('FID' + res)
+    plt.xlabel('time (s)')
+    plt.legend()
+
+    plt.subplot2grid((2, 3), (1, 0),colspan=2)
+    plt.plot(omega_vals, np.real(signal2), label = 'real' )
+    plt.plot(omega_vals, np.imag(signal2), 'r', label = "imaginary" )
+    plt.title('NMR Signal' + res)
+    plt.xlabel('frequency (Hz)')
+    plt.legend()
+
+    plt.subplot2grid((2, 3), (0, 2), rowspan=2)
+    plt.axis("off")
+    plt.text(0.2,0.95,"Input Values:")
+    button = Button(plt.axes([0.72, 0.07, 0.23, 0.1]), 'Generate Spectrum', color="lightgoldenrodyellow", hovercolor='c')
+
+    
+
+    var_list = [str(var.LAMBDA),str(var.M0),str(np.degrees(var.ALPHA)),str(np.degrees(var.OMEGA)),str(var.R2)]
+    var_names = ["Lambda: ","M Naught: ","Alpha: ","Omega: ","R2: "]
+    separation = 0.0
+    text_boxes = []
+    for i in range (len(var_list)):
+        text_boxes.append(TextBox(plt.axes([.80, 0.7-separation, 0.1, 0.07]),var_names[i], initial = var_list[i]))
+        separation += 0.12
+    func_list = [set_lambda,set_M0,set_aplha,set_omega,set_R2]
+    for i in range (len(var_list)):
+        text_boxes[i].on_submit(func_list[i])
+    button.on_clicked(plot)
+    plt.subplots_adjust(hspace = 0.6)
+    plt.show() 
+
+def set_lambda(text):
+    var.LAMBDA = float(text)
+def set_M0(text):
+    var.M0 = float(text)
+def set_aplha(text):
+    var.ALPHA = np.radians(float(text))
+def set_omega(text):
+    var.OMEGA = np.radians(float(text))
+def set_R2(text):
+    var.R2 = float(text)
+
+
 user_var_input()
-plot_time()
-plot_frequency()
+
+
 
 
